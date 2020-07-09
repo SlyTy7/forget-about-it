a859306d463aced29ed9388c84db279b9ca03219
Tyler West <tyler.j.west.7@gmail.com>
2020-07-08 22:21:06 -0700

    build basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Header from "./Header.js";
+import Body from "./Body.js";
+import Footer from "./Footer.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
