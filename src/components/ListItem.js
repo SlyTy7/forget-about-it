7ef68bcc6a17d8f99ccb97866a4fd298419dadde
Tyler West <tyler.j.west.7@gmail.com>
2020-07-15 12:27:53 -0700

    add basic styles to items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "../styles/ListItem.css";
 
 class ListItem extends React.Component {
+
+  getClassNames = () => {
+    if(this.props.completed){
+      return "list-item done-item"
+    } else {
+      return "list-item todo-item"
+    }
+  }
+
   render() {
     return (
-      <li onClick={this.props.handleClick}>{this.props.value}</li>
+      <li className={this.getClassNames()} onClick={this.props.handleClick}>{this.props.value}</li>
     )
   }
 }
